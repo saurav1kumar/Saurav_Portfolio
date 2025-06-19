8234985e7e31d1773c231a101ff7d6ac73539ba0
saurav1kumar <blacksaura2@gmail.com>
2025-06-19 14:43:21 +0530

    Fix deployment with GitHub Actions workflow

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Saurav_Portfolio/' : '/',
+  base: '/Saurav_Portfolio/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
